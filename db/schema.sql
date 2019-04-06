7125022cf2c039ffc92e30c08688276d63fb2341
Bronson <bt98199@gmail.com>
2019-04-05 21:32:45 -0700

    blunt strokes

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+drop database if exists burgers_db;
+create database burgers_db;
+use burgers_db;
+create table burgers
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(50) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+);
+select * from burgers;
+
+-- \i  seeds.sql;
